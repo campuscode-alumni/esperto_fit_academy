efc2c6b1ed21ae5b98dd040c6670eb6b8bd57de8
Ana e Vinicius <treinadev+par08@campuscode.com.br>
2019-09-03 16:58:20 -0300

    WIP adicionando validações
    Co-authored-by: Vinícius <vinicius_cunha2000@hotmail.com>
    Co-authored-by: Ana <anapdubas@gmail.com>

--- spec/factories/gyms.rb
+++ spec/factories/gyms.rb
@@ -1,5 +1,6 @@
 FactoryBot.define do
   factory :gym do
-    
+    sequence :name :cod :adress :open_hour :close_hour :working_days do |n|
+      
   end
 end
--- spec/features/employee_view_home_page_spec.rb
+++ spec/features/employee_view_home_page_spec.rb
@@ -18,11 +18,31 @@ feature 'employee view home page' do
     fill_in 'Horário de abertura', with: '09:00'
     fill_in 'Horário de fechamento', with: '23:00'
     fill_in 'Dias de funcionamento', with: 'Segunda à Sexta'
+    #attach_file 'Galeria', Rails.root.join('spec', 'support', 'unidade.jpg')
     click_on 'Enviar'
 
     expect(page).to have_content('Academia cadastrada')
     expect(page).to have_css('h3', text: 'Academia Paulista')
     expect(page).to have_css('p', text: 'Av Paulista, 123')
+    #expect(page).to have_css('img[src*="unidade.jpg"]')
     expect(page).not_to have_link('Enviar')
+  end
+
+  scenario 'and fields must be unique' do 
+    visit root_path
+    
+    click_on 'Cadastrar Academia'
+
+    fill_in 'Nome', with: 'Academia Paulista'
+    fill_in 'Código', with: '001'
+    fill_in 'Endereço', with: 'Av Paulista, 123'
+    fill_in 'Horário de abertura', with: '09:00'
+    fill_in 'Horário de fechamento', with: '23:00'
+    fill_in 'Dias de funcionamento', with: 'Segunda à Sexta'
+    #attach_file 'Galeria', Rails.root.join('spec', 'support', 'unidade.jpg')
+    click_on 'Enviar'
+
+    expect(page).to have_content('Nome já em uso')
+    expect(page).not_to have_css('h3', text: 'Academia Paulista')
   end
 end
