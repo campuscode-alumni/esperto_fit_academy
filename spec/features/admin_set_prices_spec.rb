586fe552e475aa8df675c91c393c1480bd961576
Alan e Ciro <treinadev+par01@campuscode.com.br>
2019-09-09 16:33:52 -0300

    WIP iniciado teste para Referenciar Precos

---
+++ spec/features/admin_set_prices_spec.rb
@@ -0,0 +1,15 @@
+require 'rails_helper'
+
+feature 'Admin set prices' do
+  scenario 'successfully' do
+    # arrange
+    admin = create(:employee, admin: true)
+    plan = create(:plan)
+    gym = create(:gym)
+
+    # act
+    
+
+
+  end
+end
