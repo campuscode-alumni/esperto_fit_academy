87ebfd9c012cda605254d3d917e65e003237b693
Alan e Ciro <treinadev+par01@campuscode.com.br>
2019-09-11 13:50:05 -0300

    API de obter todas as academias
    Co-authored-by: Thiago <thiagodarochagarcia@gmail.com>
    Co-authored-by: Ana <anapdubas@gmail.com>

--- app/controllers/api/v1/gyms_controller.rb
+++ app/controllers/api/v1/gyms_controller.rb
@@ -1,6 +1,13 @@
 class Api::V1::GymsController < Api::V1::ApiController
   def index
     @gyms = Gym.all 
-    render json: @gyms, status: 200
+    if @gyms.empty?
+      render json:  {messages:'Nenhuma unidade cadastrada'}, status: :not_found
+    else
+      render json: @gyms, status: 200
+    end
+    
+    
+    
   end
 end
