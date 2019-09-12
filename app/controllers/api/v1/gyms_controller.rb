0670f86f49fc0c877ab55a758855d959220d8d92
Ana e Vinicius <treinadev+par08@campuscode.com.br>
2019-09-12 13:58:39 -0300

    Add image to api show gym details (done)
    Co-authored-by: Vinicius <vinicius_cunha2000@hotmail.com>
    Co-authored-by: Alan <alankhiga@gmail.com>

--- app/controllers/api/v1/gyms_controller.rb
+++ app/controllers/api/v1/gyms_controller.rb
@@ -1,8 +1,17 @@
 class Api::V1::GymsController < Api::V1::ApiController
+
   def show
     @gym = Gym.find(params[:id])
-    render json: @gym, status: 200
+
+    render  json: {gym: @gym.as_json.merge(images: build_images)}, status: 200
   rescue ActiveRecord::RecordNotFound
     render json: { message: 'Academia não encontrada'}, status: 404
   end
+
+  private
+
+  def build_images
+    @gym.gallery.map {|image| url_for(image)}
+  end
+
 end
--- spec/requests/show_gym_details_spec.rb
+++ spec/requests/show_gym_details_spec.rb
@@ -8,13 +8,14 @@ describe 'show gym details ' do
     json_gym = JSON.parse(response.body, symbolize_names: true)
 
     expect(response.status).to eq 200
-    expect(json_gym[:cod]).to eq gym.cod
-    expect(json_gym[:open_hour]).to eq gym.open_hour
-    expect(json_gym[:close_hour]).to eq gym.close_hour
-    expect(json_gym[:working_days]).to eq gym.working_days
-    expect(json_gym[:address]).to eq gym.address
+    expect(json_gym[:gym][:cod]).to eq gym.cod
+    expect(json_gym[:gym][:open_hour]).to eq gym.open_hour
+    expect(json_gym[:gym][:close_hour]).to eq gym.close_hour
+    expect(json_gym[:gym][:working_days]).to eq gym.working_days
+    expect(json_gym[:gym][:address]).to eq gym.address
+    expect(json_gym[:gym][:images].first).to eq "http://www.example.com/rails/active_storage/blobs/eyJfcmFpbHMiOnsibWVzc2FnZSI6IkJBaHBCZz09IiwiZXhwIjpudWxsLCJwdXIiOiJibG9iX2lkIn19--f38dc1ef0ace3dbcd4f962ceffdf6ffa578a23f1/academia_01.jpeg"
   end
-
+  
   it 'returns an error if the gym is not found' do
     get "/api/v1/gyms/1111"
     json_gym = JSON.parse(response.body, symbolize_names: true)
