fa8a86a28a4950319a4602781060a2c857f4f344
Ana e Vinicius <treinadev+par08@campuscode.com.br>
2019-09-17 15:46:38 -0300

    Endpoint para visualizar os planos da acedemia
    Co-authored-by: Vinicius <vinicius_cunha2000@hotmail.com>
    Co-authored-by: Alan <alankhiga@gmail.com>

--- app/controllers/api/v1/plans_controller.rb
+++ app/controllers/api/v1/plans_controller.rb
@@ -1,12 +1,18 @@
 class Api::V1::PlansController < Api::V1::ApiController
   def index
     @gym = Gym.find(params[:gym_id])
-    @price = Price.where(gyms: @gym)
     @plans = @gym.plans
-    if @gym.nil?
-      render json:  {messages:'Nenhum plano cadastrado'}, status: :not_found
-    else
-      render json: @plans, status: 200, include: ['prices']
+    render json: {plans: build_gym_plans(@plans, @gym)}, status: 200
+  rescue ActiveRecord::RecordNotFound  
+    render json:  {messages:'Nenhum plano cadastrado'}, status: :not_found
+  end
+
+  private
+
+  def build_gym_plans(plans, gym)
+    plans.map do |plan|
+      price = Price.find_by(plan: plan, gym: gym).value
+      plan.as_json.merge(price: price)
     end
   end
 end
--- spec/requests/api_show_all_gym_plans_prices_spec.rb
+++ spec/requests/api_show_all_gym_plans_prices_spec.rb
@@ -7,18 +7,30 @@ describe 'api show all gym plans prices' do
     other_gym = create(:gym, name: 'Academia CampusCode')
     plan = create(:plan)
     other_plan = create(:plan, name: 'Basic')
-    price = create(:price, value: 100000, gym_id: gym.id, plan_id: other_plan.id )
-    other_price = create(:price, value: 500000, gym_id: gym.id, plan_id: plan.id )
+    price = create(:price, value: 100000, gym_id: gym.id, plan_id: plan.id )
+    other_price = create(:price, value: 500000, gym_id: gym.id, plan_id: other_plan.id )
     another_price = create(:price, gym_id: other_gym.id, plan_id: plan.id)
 
     # act
     get "/api/v1/gyms/#{gym.id}/plans"
     json_prices = JSON.parse(response.body, symbolize_names: true)
+
     # assert
     expect(response.status).to eq 200
-    expect(json_prices[:name]).to eq gym.name
-    expect(json_prices[:plans][1][:name]).to eq plan.name
-    expect(json_prices[:plans][0][:name]).to eq other_plan.name
+    expect(json_prices[:plans][0][:name]).to eq plan.name
+    expect(json_prices[:plans][0][:price]).to eq price.value
+    expect(json_prices[:plans][1][:name]).to eq other_plan.name
+    expect(json_prices[:plans][1][:price]).to eq other_price.value
     expect(json_prices[:plans].count).to eq (2)
+  end
+
+  it 'fails' do
+    
+    get "/api/v1/gyms/100/plans"
+
+    json_prices = JSON.parse(response.body, symbolize_names: true)
+
+    expect(response.status).to eq 404
+    expect(json_prices[:messages]).to eq 'Nenhum plano cadastrado'
   end
 end
