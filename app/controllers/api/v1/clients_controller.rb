9328c8ac34ee1232b13fdf0bf8ab42e58f93bb9d
Henrique Morato <henrique.morato@campuscode.com.br>
2019-09-18 17:34:57 -0300

    Consulta de Cpf

--- app/controllers/api/v1/clients_controller.rb
+++ app/controllers/api/v1/clients_controller.rb
@@ -2,11 +2,11 @@ class Api::V1::ClientsController < Api::V1::ApiController
 
   def consult_cpf
     @client = Client.find_by(cpf: params[:cpf])
-    return render json: 'CPF não encontrado', status: 404 unless @client
+    return render json: { message: 'CPF não encontrado' }, status: 404 unless @client
 
     render json: @client.to_json(only: [:cpf, :status]), status: 302
   end
-  
+
   def create
     @client = Client.new(client_params)
     if @client.save
@@ -15,26 +15,26 @@ class Api::V1::ClientsController < Api::V1::ApiController
       render json: { message: "Não foi possivel cadastrar esse aluno", errors: @client.errors.full_messages }, status: :precondition_failed
     end
   end
-  
+
   def show
-		@client = Client.find_by(cpf: params[:cpf])
-		return render json: 'Cliente não encontrado', status: 404 unless @client
-    
+    @client = Client.find_by(cpf: params[:cpf])
+    return render json: { message: 'Cliente não encontrado' }, status: 404 unless @client
+
     render json: @client, status: 302
   end
 
   def inactivate
     @client = Client.find_by(cpf: params[:cpf])
-    return render json: 'CPF não encontrado', status: 404 unless @client
+    return render json: { message: 'CPF não encontrado' }, status: 404 unless @client
 
     @client.inactive! if @client.active?
     render json: @client.to_json(only: [:cpf, :status]), status: 202
   end
 
-	private
+  private
 
-	def client_params
-		params.require(:client).permit(:name, :cpf, :email, :gym_id, :plan_id)
-	end
-	
+  def client_params
+    params.require(:client).permit(:name, :cpf, :email, :gym_id, :plan_id)
+  end
+
 end
