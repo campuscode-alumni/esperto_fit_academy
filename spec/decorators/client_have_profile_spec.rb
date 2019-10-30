e69b6971ecbcc19bd5b4521cae71d3279d53c0b0
Vinicius_Cunha <vinicius_cunha2000@hotmail.com>
2019-10-30 19:09:33 -0300

    WIP DECORATOR TESTS

--- spec/decorators/client_have_profile_spec.rb
+++ spec/decorators/client_have_profile_spec.rb
@@ -2,6 +2,7 @@ require 'rails_helper'
 describe ClientDecorator do
 
   describe '.have_profile' do
+    before{ load_profile_junior_mock }
     context 'when client have a profile' do
       subject { described_class.new(client).have_profile(client) }
       let (:client) { create(:client) }
@@ -15,7 +16,7 @@ describe ClientDecorator do
       subject { described_class.new(client).have_profile(nil) }
       let(:client) { create(:client) }
       it 'returns partial message dont have profile' do
-        expect(subject.profile).to eq("clients/partial2")
+        expect(subject).to eq("clients/partial2")
       end
     end
   end
--- spec/support/profile_api.rb
+++ spec/support/profile_api.rb
@@ -11,5 +11,18 @@ module ProfileApi
           to_return(status: 202, body: File.read(Rails.root.join('spec', 'support', 'profile.json')),
                      headers: { 'Content-Type' => 'aplication/json' })
   end
+
+  def load_profile_junior_mock
+    stub_request(:get, "http://0.0.0.0:4000/api/v1/search/customer?document=385.093.321-01").
+    with(
+      headers: {
+      'Accept'=>'*/*',
+      'Accept-Encoding'=>'gzip;q=1.0,deflate;q=0.6,identity;q=0.3',
+      'Content-Type'=>'application/json',
+      'User-Agent'=>'Faraday v0.15.4'
+      }).
+    to_return(status: 202, body: File.read(Rails.root.join('spec', 'support', 'profile.json')),
+               headers: { 'Content-Type' => 'aplication/json' })
+  end
 end
          
